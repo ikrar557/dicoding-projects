284fec1704b3d8597c0f41952c338f6bb3a58e55
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-02-25 22:52:31 +0700

    add 'filesystem' folder to complete the given task

---
+++ filesystem/index.js
@@ -0,0 +1,13 @@
+// TODO: tampilkan teks pada notes.txt pada console.
+
+const fs = require('fs');
+
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+}
+
+fs.readFile('filesystem/notes.txt', 'UTF-8', fileReadCallback);
